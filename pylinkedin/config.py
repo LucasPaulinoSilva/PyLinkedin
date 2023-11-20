f3d6e12183490e31830c95ada835150f81d137f9
Lucas Paulino da Silva <104040026+LucasPaulinoSilva@users.noreply.github.com>
2023-11-20 11:09:10 -0300

    PyLinkedin v1.0.py

--- pylinkedin/config.py
+++ pylinkedin/config.py
@@ -1,12 +1,12 @@
-# Autor: Lucas Paulino da Silva
-# Última atualização: 16/11/2023
+# Autor: 
+# Última atualização:
 
 # Dados Linkedin
-URL_CUSTOM = 'https://www.linkedin.com/jobs/search?keywords=RPA&location=Brasil&locationId=&geoId=106057199&f_TPR=r86400&f_WT=2&position=1&pageNum=0'
+URL_CUSTOM = ''
 
 # Dados pessoais Telegram
-TOKEN = '6696571994:AAG2jXS7U78bDM1PUq_bCh1AAkORo8McHxU'
-CHAT_ID = '1044837985'
+TOKEN = ''
+CHAT_ID = ''
 
 # Dados API Telegram
 URL_BASE = 'https://api.telegram.org/bot{token}/sendMessage'
